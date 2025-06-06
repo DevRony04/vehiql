8cc308ff47e960699864618af87b32f96024e5c6
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-06-06 13:14:30 +0530

    prep for deployment

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WaitlistPage = () => {
+    return (
+        <div className='mt-20 fixed w-full'>
+            <div className='aspect-video w-full'>
+                <iframe style={{width:"100%"}} height="569" src="https://www.create.xyz/app/edfac0e6-bc70-42e5-a20f-a4f6b50083a0" title="Waiting List Landing Page" frameBorder="0"></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default WaitlistPage
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mlbmgvlpbnkzsvcuxhyb.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self'https://rony.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
